fe34a6ec9939917288443968ec9be4cd87c70c25
ImranHossain5722 <imd572258@gmail.com>
2022-03-01 09:57:24 +0600

    working show details phone part

--- Js/app.js
+++ Js/app.js
@@ -19,15 +19,44 @@ const showDisplay =(sendPhoneData)=>{
         <div class="card-body">
         <h5 class="card-title fs-5 fw-bold">Brand Name: ${phone.brand}</h5>  
         <h5 class="card-title fs-5 fw-bold"> Phone Name: ${phone.phone_name}</h5>
-        <a href="#" class="btn btn-success ">Phone Details </a>
+        <button href="#" onclick="PhoneDetails('${phone.slug}')" class="btn btn-success ">Phone Details</button>
         </div>
       </div>
-       `
-       mainDiv.appendChild(div)
+      `
+      mainDiv.appendChild(div)
+      
 
-
-
+     
     }
+
+}
+
+// phone details
+ const PhoneDetails=(slug) =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${status.slug}`
+        fetch(url)
+        .then(response=> response.json())
+        .then(dataLoad =>displayPhone(dataLoad.data.slug))
+
+ }
+
+//  show UI phone details
+const displayPhone =(detailsLoad)=>{
+
+    const phoneDetails=document.getElementById('phoneDetails')
+    const div = document.createElement('div')
+    div.innerHTML=`
+    <div class="shadow rounded-3 border border-success card mt-2 mb-4 " style="width:15rem; ">
+    <img src="${detailsLoad.image}" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+    <h5 class="card-title fs-5 fw-bold">Brand Name: ${detailsLoad.brand}</h5>  
+    <h5 class="card-title fs-5 fw-bold"> Phone Name: ${detailsLoad.phone_name}</h5>
+    </div>
+    </div>
+    
+ `
+ phoneDetails.appendChild(div)
+ console.log(detailsLoad);
 
 
 }
