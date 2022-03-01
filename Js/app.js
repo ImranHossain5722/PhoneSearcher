50a597c03aff9a92d8ebdafdff73f7d63acfdf5d
ImranHossain5722 <imd572258@gmail.com>
2022-03-01 08:29:13 +0600

    phone data loading well

---
+++ Js/app.js
@@ -0,0 +1,33 @@
+const searchPhone =()=>{
+    const phoneSearcher = document.getElementById('input-id').value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearcher}`
+     fetch(url)
+    .then(response => response.json())
+    .then (data => showDisplay(data.data))
+
+}
+const showDisplay =(sendPhoneData)=>{
+
+    const mainDiv = document.getElementById('parantaDiv')
+
+    for (const phone of sendPhoneData ){
+        const div=document.createElement('div')
+        div.className =' col-12 col-md-4 col-lg-4 '
+        div.innerHTML =`
+        <div class="shadow rounded-3 border border-success card mt-2 mb-4 " style="width:15rem; ">
+        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+        <div class="card-body">
+        <h5 class="card-title fs-5 fw-bold">Brand Name: ${phone.brand}</h5>  
+        <h5 class="card-title fs-5 fw-bold"> Phone Name: ${phone.phone_name}</h5>
+        <a href="#" class="btn btn-success ">Phone Details </a>
+        </div>
+      </div>
+       `
+       mainDiv.appendChild(div)
+
+
+
+    }
+
+
+}
