43fdff20e7202236a17f9207dff4f58b2ba52a55
ImranHossain5722 <imd572258@gmail.com>
2022-03-02 17:39:31 +0600

    done

--- Js/app.js
+++ Js/app.js
@@ -1,60 +1,60 @@
 // Api load  
-const searchPhone =()=>{
+const searchPhone = () =>{
     document.getElementById('parantDiv').innerHTML=" ";
     document.getElementById('phoneDetails').innerHTML=" ";
-    
     const error = document.getElementById('error')
-    const phoneSearcher = document.getElementById('input-id').value
-                //error handler
-        if(phoneSearcher.length == 0 || phoneSearcher==""
-            
-             ){
-
-           error.innerText="*please type your phone name"
-           
-        
-        }else if(phoneSearcher <= 0 || phoneSearcher > 0 ){
+    const phoneSearcher = document.getElementById('input-id').value  
+       // console.log(phoneSearcher.data);   
+        //error handler
+        if( phoneSearcher==""){
+         error.innerText ="*please type your phone name"
+         document.getElementById('input-id').value=""
+         
+        }else if(phoneSearcher <= 0 || phoneSearcher >0){
             error.innerText="please type your phone name"
-            
+            document.getElementById('input-id').value="";
         }else{
             const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearcher}`
             fetch(url)
-           .then(response => response.json())
+            .then(response => response.json())
             .then (data => showDisplay(data.data))
-            //.then(data => console.log(data.data[0]) )
+            //.then(data => console.log(data.data) )
            document.getElementById('input-id').value="";
-
-              
-
+           error.innerText=""
         }
-
-
 
 }
 // Api data show ui 
 const showDisplay =(sendPhoneData)=>{
-    const mainDiv = document.getElementById('parantDiv')
-    const showFirst20Data =sendPhoneData.slice(0,20)
-// spinner function
-    showSpinner('block');
+    if(sendPhoneData == 0 ){
+        error.innerText ="SORRY invalid name ! PLEASE Try a valid name"
 
-    for (const phone of showFirst20Data ){
-        const div=document.createElement('div')
-        div.className =' col-12 col-md-4 col-lg-4 '
-        div.innerHTML =`
-        <div class=" my-card shadow-lg rounded-3 border border-success card mt-2 mb-4 " style="width:15rem; ">
-        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">
-        <h5 class="card-title fs-5 fw-bold">Brand Name: ${phone.brand}</h5>  
-        <h5 class="card-title fs-5 fw-bold"> Product Name: ${phone.phone_name}</h5>
-        <button href="#" onclick="PhoneDetails('${phone.slug}')" class="btn btn-success ">Phone Details</button>
-        </div>
-      </div>
-      `
-      mainDiv.appendChild(div)
-     
+    }else{
+
+        const mainDiv = document.getElementById('parantDiv')
+        const showFirst20Data =sendPhoneData.slice(0,20)
+    // spinner function
+        showSpinner('block');
+        for (const phone of showFirst20Data ){
+            const div=document.createElement('div')
+            div.className =' col-12 col-md-4 col-lg-4 '
+            div.innerHTML =`
+            <div class=" my-card shadow-lg rounded-3 border border-success card mt-2 mb-4 " style="width:15rem; ">
+            <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+            <div class="card-body">
+            <h5 class="card-title fs-5 fw-bold">Brand Name: ${phone.brand}</h5>  
+            <h5 class="card-title fs-5 fw-bold"> Product Name: ${phone.phone_name}</h5>
+            <button href="#" onclick="PhoneDetails('${phone.slug}')" class="btn btn-success ">Phone Details</button>
+            </div>
+          </div>
+          `
+          mainDiv.appendChild(div)
+         
+        }
+        showSpinner('none');  
+    
     }
-    showSpinner('none');  
+  
   
  
 }
